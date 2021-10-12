d8ca44714c42e96ff3d3c9b1db50e17dd4c37ce1
azarrias <adolfo.zarrias@gmail.com>
2021-10-12 15:18:08 +0200

    Add AdventureGame to manage game states and text

---
+++ Choose your adventure/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI storyText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
